9d7aaf81a6ec4b5b4f897f578adf04abca230130
jaemin <lemon__96@naver.com>
2025-10-15 23:53:52 +0900

    feat: basic blog setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://example.com",
+  integrations: [preact()],
 });
